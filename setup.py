7e748820e50b872b9a2ceb3848c307e0de4109f2
vmanamino <vmanamino@gmail.com>
2017-02-06 17:17:14 +0000

    testing dir

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bod_metadata_management',
+      version='0.1',
+      description='modules, libraries, and scripts to manage metadata for Boston Open Data',
+      url='https://github.com/vmanamino/bod_metadata_management',
+      author='Viral Amin',
+      author_email='',
+      license='MIT',
+      packages=['bod_metadata_management'],
+      zip_safe=False)
